18e6e5380f653c9fb735de96cfeb25b22edd7f58
Francisco Sales <franciscofsales1@gmail.com>
2017-04-21 00:12:07 +0100

    callout example

---
+++ CalloutView/Still.js
@@ -0,0 +1,127 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Animated,
+	Dimensions,
+	TouchableWithoutFeedback
+} from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export default class Still extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			scale: new Animated.Value(1)
+		};
+	}
+	componentWillMount() {
+		this.bgFadeInterpolate = this.state.scale.interpolate({
+			inputRange: [0.9, 1],
+			outputRange: ['rgba(0,0,0,.3)', 'rgba(0,0,0,0)']
+		});
+		this.textFade = this.state.scale.interpolate({
+			inputRange: [0.9, 1],
+			outputRange: [0, 1]
+		});
+		this.calloutTranslate = this.state.scale.interpolate({
+			inputRange: [0.9, 1],
+			outputRange: [0, 150]
+		});
+	}
+
+	handlePress = async () => {
+		await Animated.timing(this.state.scale, {
+			toValue: this.props.focused ? 1 : .9,
+			duration: 300
+		}).start()
+		this.props.onFocus(!this.props.focused);
+	};
+	render() {
+		const animatedStyle = {
+			transform: [
+				{ translateX: this.props.translateX },
+				{ scale: this.state.scale }
+			]
+		};
+		const bgFadeStyle = {
+			backgroundColor: this.bgFadeInterpolate
+		};
+		const textFadeStyle = {
+			opacity: this.textFade
+		};
+		const calloutStyle = {
+			transform: [
+				{
+					translateY: this.calloutTranslate
+				}
+			]
+		};
+		return (
+			<View style={styles.container}>
+				<View>
+					<View style={styles.container}>
+						<Animated.Image
+							source={this.props.image}
+							style={[animatedStyle, styles.image]}
+							resizeMode="cover"
+						/>
+						<TouchableWithoutFeedback onPress={this.handlePress}>
+							<Animated.View
+								style={[StyleSheet.absoluteFill, styles.center, bgFadeStyle]}
+							>
+								<Animated.View style={[styles.textWrap, textFadeStyle]}>
+									<Text style={styles.title}>{this.props.title}</Text>
+								</Animated.View>
+							</Animated.View>
+						</TouchableWithoutFeedback>
+						<Animated.View style={[styles.callout, calloutStyle]}>
+							<View>
+								<Text style={styles.title}>{this.props.title}</Text>
+							</View>
+						</Animated.View>
+					</View>
+				</View>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		width,
+		height,
+		overflow: 'hidden'
+	},
+	image: {
+		flex: 1,
+		width: null,
+		height: null
+	},
+	center: {
+		justifyContent: 'center'
+	},
+	textWrap: {
+		backgroundColor: 'rgba(0, 0, 0, 0.5)',
+		paddingVertical: 10
+	},
+	title: {
+		backgroundColor: 'transparent',
+		fontSize: 30,
+		color: '#FFF',
+		textAlign: 'center'
+	},
+	callout: {
+		height: 150,
+		backgroundColor: 'rgba(0,0,0,.5)',
+		justifyContent: 'center',
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right: 0
+	}
+});
---
+++ CalloutView/index.ios.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	Dimensions,
+	Animated
+} from 'react-native';
+import Still from './Still';
+
+const { height, width } = Dimensions.get('window');
+const Images = [
+	{ image: require('./images/image1.jpg'), title: 'Amazing Cocktail' },
+	{ image: require('./images/image2.jpg'), title: 'Drinkme Whole' },
+	{ image: require('./images/image3.jpg'), title: 'Incredible SexOnTheBeach' },
+	{ image: require('./images/image4.jpg'), title: 'Mojito or Margherita' }
+];
+const getInterpolate = (animatedScroll, i, imagesLength) => {
+	const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+	const outputRange = i === 0 ? [0, 0, 150] : [-300, 0, 150];
+	return animatedScroll.interpolate({
+		inputRange,
+		outputRange,
+		extrapolate: 'clamp'
+	});
+};
+const getSeparator = i => (
+	<View style={[styles.separator, { left: (i - 1) * width - 2.5 }]} key={i} />
+);
+export default class ParallaxScrollView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			animatedScroll: new Animated.Value(0),
+			scrollEnabled: true
+		};
+	}
+
+	handleFocus = focused => {
+		this.setState({
+			scrollEnabled: !focused
+		});
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<ScrollView
+					pagingEnabled
+					horizontal
+					scrollEnabled={this.state.scrollEnabled}
+					scrollEventThrottle={16}
+					onScroll={Animated.event([
+						{
+							nativeEvent: {
+								contentOffset: {
+									x: this.state.animatedScroll
+								}
+							}
+						}
+					])}
+				>
+					{Images.map((image, i) => (
+						<Still
+							key={i}
+							{...image}
+							onFocus={this.handleFocus}
+							focused={!this.state.scrollEnabled}
+							translateX={getInterpolate(
+								this.state.animatedScroll,
+								i,
+								Images.length
+							)}
+						/>
+					))}
+					{Array.apply(null, { length: Images.length + 2 }).map((_, i) =>
+						getSeparator(i)
+					)}
+				</ScrollView>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#333'
+	},
+	separator: {
+		backgroundColor: '#000',
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		width: 5
+	}
+});
+
+AppRegistry.registerComponent('ParallaxScrollView', () => ParallaxScrollView);
