2e8be5dbe33ab2240b762e281ae94543213a2428
Francisco Sales <franciscofsales1@gmail.com>
2017-04-21 09:03:04 +0100

    exploding hearts

--- HeartShpaedBounceButton/index.ios.js
+++ HeartShpaedBounceButton/index.ios.js
@@ -15,6 +15,45 @@ import {
 } from 'react-native';
 import Heart from './Heart';
 
+const getTransformationAnimation = (
+	animation,
+	scale,
+	y,
+	x,
+	rotate,
+	opacity
+) => {
+	const scaleAnimation = animation.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, scale]
+	});
+	const xAnimation = animation.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, x]
+	});
+	const yAnimation = animation.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, y]
+	});
+	const rotateAnimation = animation.interpolate({
+		inputRange: [0, 1],
+		outputRange: ['0deg', rotate]
+	});
+	const opacityAnimation = animation.interpolate({
+		inputRange: [0, 1],
+		outputRange: [0, opacity]
+	});
+	return {
+		opacity: opacityAnimation,
+		transform: [
+			{ scale: scaleAnimation },
+			{ translateX: xAnimation },
+			{ translateY: yAnimation },
+			{ rotate: rotateAnimation }
+		]
+	};
+};
+
 export default class HeartShpaedBounceButton extends Component {
 	constructor(props) {
 		super(props);
@@ -40,13 +79,33 @@ export default class HeartShpaedBounceButton extends Component {
 			liked: !this.state.liked,
 			animating: true
 		});
-		Animated.spring(this.state.scale, {
-			toValue: 2,
-			friction: 5
-		}).start(async () => {
-			await this.setState({ animating: false });
-			this.state.scale.setValue(0);
-		});
+    const showAnimations = this.state.animations.map((animation) => {
+      return Animated.spring(animation, {
+        toValue: 1,
+        friction: 4
+      })
+    })
+    const hideAnimations = this.state.animations.map((animation) => {
+      return Animated.timing(animation, {
+        toValue: 0,
+        duration:50
+      })
+    }).reverse()
+    Animated.parallel([
+    	Animated.spring(this.state.scale, {
+    		toValue: 2,
+    		friction: 4
+    	}),
+    	Animated.sequence([
+    		Animated.stagger(50, showAnimations),
+    		Animated.delay(100),
+    		Animated.stagger(50, hideAnimations)
+    	])
+    ]).start(async () => {
+    	await this.setState({ animating: false });
+    	this.state.scale.setValue(0);
+    });
+
 	};
 	render() {
 		const bouncyHeart = this.state.scale.interpolate({
@@ -58,11 +117,19 @@ export default class HeartShpaedBounceButton extends Component {
 		};
 		return (
 			<View style={styles.container}>
-				<TouchableWithoutFeedback onPress={this.triggerLike}>
-					<Animated.View style={heartButtonStyle}>
-						<Heart filled={this.state.liked} />
-					</Animated.View>
-				</TouchableWithoutFeedback>
+        <View>
+  				<Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[5], .4, -280, 0, "10deg", .7)]} />
+  				<Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[4], .7, -120, 40, "45deg", .5)]} />
+  				<Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[3], .8, -120, 40, "-45deg", .3)]} />
+  				<Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[2], .3, -150, 120, "-35deg", .6)]} />
+  				<Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[1], .3, -120, -120, "-35deg", .7)]} />
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[0], .8, -60, 0, "35deg", .8)]} />
+          <TouchableWithoutFeedback onPress={this.triggerLike}>
+  					<Animated.View style={heartButtonStyle}>
+  						<Heart filled={this.state.liked} />
+  					</Animated.View>
+  				</TouchableWithoutFeedback>
+        </View>
 			</View>
 		);
 	}
@@ -74,6 +141,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: '#FFF'
+	},
+	heart: {
+		position: 'absolute',
+		top: 0,
+		left: 0
 	}
 });
 
